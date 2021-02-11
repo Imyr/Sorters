af56987e1350499b5106dcef95c04502c82ba8b2
Shashwat Verma <68842852+Imyr@users.noreply.github.com>
2021-02-11 14:48:04 +0530

    Update The 'New' and 'Improved' Anime Sorter.py

--- The 'New' and 'Improved' Anime Sorter.py
+++ The 'New' and 'Improved' Anime Sorter.py
@@ -1,8 +1,9 @@
 import os
 import shutil
+import time
 
-Source_Path = "B:/Anime/Airing/"
-Destination_Path = "B:/Anime/Auto Synced/"
+Source_Path = "H:/Anime/Airing/"
+Destination_Path = "H:/Anime/Auto Synced/"
 
 Filenames = os.listdir(Source_Path)
 
@@ -12,20 +13,27 @@ def UserExtractor(Filename):
 
 def TitleExtractor(Filename):
     ReverseName = Filename[::-1]
-    Title = "rectification_required"
-    if ReverseName.find(" - ") != -1:
-        Title = ""
+    Title = ""
+    if Filename.find(" - ") != -1:
         Title = Filename[Filename.find("]")+2:(len(Filename) - ReverseName.find(" - ")) - 3]
+    elif Filename.find(" [") != -1:
+        Filename = Filename[Filename.find("]")+2:]
+        Title = Filename[:Filename.find("[")-1]
+    elif Filename.find(" (") != -1:
+        Filename = Filename[Filename.find("]")+2:]
+        Title = Filename[:Filename.find("(")-1]
     return (Title)
 
 def QualityExtractor(Filename):
-    Quality = "rectification_required"
+    Quality = ""
     if Filename.find("1080") != -1 or Filename.find("1980") != -1:
         Quality = "1080p"
     if Filename.find("720") != -1 or Filename.find("1280") != -1:
         Quality = "720p"
     if Filename.find("480") != -1:
         Quality = "480p"
+    if Filename.find("540") != -1:
+        Quality = "540p"
     if Filename.find("360") != -1:
         Quality = "360p"
     return (Quality)
@@ -40,12 +48,17 @@ def CheckerMover(Filename):
     QualityDirectory = os.path.join(Destination_Path, UserExtractor(Filename), TitleExtractor(Filename), QualityExtractor(Filename))
     if not os.path.exists(QualityDirectory):
         os.mkdir(QualityDirectory)
-    print ("Stfu, moving... " + Filename + " right now!")
     shutil.move(Source_Path + Filename, QualityDirectory)
-    print ("Done, bitch!")
-for i in Filenames:
-    CheckerMover(i)
 
+StartTime = time.time()
+
+for i in range(len(Filenames)):
+    print ("Moving " + Filenames[i] + " right now... ")
+    CheckerMover(Filenames[i])
+    print (i + 1, " of ", len(Filenames), " done!")
+
+EndTime = time.time()
+print("Finished sorting in: ", EndTime - StartTime)
  
 
 
